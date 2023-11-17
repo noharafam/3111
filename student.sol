a989dece6393b18893bb8ebb1c405068f8fc92a2
noharafam <134853625+noharafam@users.noreply.github.com>
2023-11-17 10:24:43 +0530

    Update student.sol

--- student.sol
+++ student.sol
@@ -1,61 +1,89 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+//4. Write a program in solidity to create Student data. Use the following constructs:
+//Structures
+//Arrays
+//Fallback
+//Deploy this as smart contract on Ethereum and Observe the transaction fee and Gas values.
+pragma solidity 0.8.0;
 
 // Build the Contract
-contract MarksManagmtSys {
-    // Create a structure for student details
-    struct Student {
-        int ID;
-        string fName;
-        string lName;
-        int marks;
-    }
-
+contract MarksManagmtSys
+{
+	// Create a structure for
+	// student details
+	   uint[] data; 
+    
+    // Declaring state variable
+  //  uint8 j = 0;
+    uint k;
+  
+	struct Student
+	{
+		int ID;
+		string fName;
+		string lName;
+		int marks;
+	
+	}
+	
     event Log(string func, address sender, uint value);
+	address owner;
+	int public stdCount = 0;
+	mapping(int => Student) public stdRecords;
 
-    address public owner;
-    int public stdCount = 0;
-    mapping(int => Student) public stdRecords;
-    uint[] public data;
+	modifier onlyOwner
+	{
+		require(owner == msg.sender);
+		_;
+	}
+	constructor()
+	{
+		owner=msg.sender;
+	}
 
-    modifier onlyOwner() {
-        require(owner == msg.sender, "Only the owner can call this function");
-        _;
+	// Create a function to add
+	// the new records
+
+	function addNewRecords(int _ID,
+						string memory _fName,
+						string memory _lName,
+						int _marks
+						) public onlyOwner				
+	{
+		// Increase the count by 1
+		stdCount = stdCount + 1;
+     
+		// Fetch the student details
+		// with the help of stdCount
+		stdRecords[stdCount] = Student(_ID, _fName,
+									_lName, 
+									_marks
+									);
+	
+	}
+	function percentage(uint k) public returns(uint[] memory){
+   // do{
+     //   j++;
+      data.push(k);
+    // }
+     //while(j < 5) ;
+     return data;
     }
+	
+	// Create a function to add bonus marks
+	function bonusMarks(int _bonus) public onlyOwner
+	{
+		stdRecords[stdCount].marks =
+					stdRecords[stdCount].marks + _bonus;
 
-    constructor() {
-        owner = msg.sender;
-    }
-
-    // Create a function to add the new records
-    function addNewRecords(
-        int _ID,
-        string memory _fName,
-        string memory _lName,
-        int _marks
-    ) public onlyOwner {
-        // Increase the count by 1
-        stdCount++;
-
-        // Fetch the student details with the help of stdCount
-        stdRecords[stdCount] = Student(_ID, _fName, _lName, _marks);
-    }
-
-    // Create a function to add bonus marks
-    function bonusMarks(int _bonus) public onlyOwner {
-        stdRecords[stdCount].marks += _bonus;
-    }
-
-    function percentage(uint k) public returns (uint[] memory) {
-        data.push(k);
-        return data;
-    }
-
-    fallback() external payable {
-        emit Log("fallback", msg.sender, msg.value);
-    }
-
-    receive() external payable {
-        emit Log("receive", msg.sender, msg.value);
-    }
+	}
+	fallback() external payable 
+	{
+		emit Log("fallback", msg.sender, msg.value);
+	}
+	receive() external payable 
+	{
+		emit Log("receive", msg.sender, msg.value);
+	}
 }
+
+
